a0e9047d04caaf1fed1f636782c90181bc816572
jackthoene <thoene.jack@gmail.com>
2024-04-18 12:42:10 -0500

    IPR

--- app/components/ProjectsSection.jsx
+++ app/components/ProjectsSection.jsx
@@ -1,62 +1,127 @@
-import React from "react";
+"use client";
+import React, { useState, useRef } from "react";
 import ProjectCard from "./ProjectCard";
+import ProjectTag from "./ProjectTag";
+import { motion, useInView } from "framer-motion";
 
-const ProjectsData = [
+const projectsData = [
   {
     id: 1,
     title: "React Portfolio Website",
-    description: "Project 1 Description",
+    description: "Project 1 description",
     image: "/Images/Projects/Me_sitting.jpg",
     tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
   },
   {
     id: 2,
-    title: "Photography Portfolio Website",
-    description: "Project 2 Description",
+    title: "Potography Portfolio Website",
+    description: "Project 2 description",
     image: "/Images/Projects/Me_sitting.jpg",
     tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
   },
   {
     id: 3,
-    title: "Real Time Payment Application",
-    description: "Project 3 Description",
+    title: "E-commerce Application",
+    description: "Project 3 description",
     image: "/Images/Projects/Me_sitting.jpg",
     tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
   },
   {
     id: 4,
-    title: "Smartwatch Website",
-    description: "Project 4 Description",
+    title: "Food Ordering Application",
+    description: "Project 4 description",
     image: "/Images/Projects/Me_sitting.jpg",
-    tag: ["All", "Web"],
+    tag: ["All", "Mobile"],
+    gitUrl: "/",
+    previewUrl: "/",
   },
   {
     id: 5,
-    title: "Lab Website",
-    description: "Project 5 Description",
+    title: "React Firebase Template",
+    description: "Authentication and CRUD operations",
     image: "/Images/Projects/Me_sitting.jpg",
     tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 6,
+    title: "Full-stack Roadmap",
+    description: "Project 5 description",
+    image: "/Images/Projects/Me_sitting.jpg",
+    tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
   },
 ];
 
 const ProjectsSection = () => {
+  const [tag, setTag] = useState("All");
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  };
+
+  const filteredProjects = projectsData.filter((project) =>
+    project.tag.includes(tag)
+  );
+
+  const cardVariants = {
+    initial: { y: 50, opacity: 0 },
+    animate: { y: 0, opacity: 1 },
+  };
+
   return (
-    <>
-      <h2 className="text-center text-4xl font-bold text-white mt-4">
+    <section id="projects">
+      <h2 className="text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12">
         My Projects
       </h2>
-      <div className="grid md:grid-cold-3 gap-8 md:gap-12">
-        {ProjectsData.map((project) => (
-          <ProjectCard
-            key={project.id}
-            title={project.title}
-            description={project.description}
-            imgUrl={project.image}
-            tags={project.tag}
-          />
-        ))}
+      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+        <ProjectTag
+          onClick={handleTagChange}
+          name="All"
+          isSelected={tag === "All"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Web"
+          isSelected={tag === "Web"}
+        />
+        <ProjectTag
+          onClick={handleTagChange}
+          name="Mobile"
+          isSelected={tag === "Mobile"}
+        />
       </div>
-    </>
+      <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
+        {filteredProjects.map((project, index) => (
+          <motion.li
+            key={index}
+            variants={cardVariants}
+            initial="initial"
+            animate={isInView ? "animate" : "initial"}
+            transition={{ duration: 0.3, delay: index * 0.4 }}
+          >
+            <ProjectCard
+              key={project.id}
+              title={project.title}
+              description={project.description}
+              imgUrl={project.image}
+              gitUrl={project.gitUrl}
+              previewUrl={project.previewUrl}
+            />
+          </motion.li>
+        ))}
+      </ul>
+    </section>
   );
 };
 
