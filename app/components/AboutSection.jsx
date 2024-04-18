822bcebdb5b0504bebd12dcee620f43a2d01c730
jackthoene <thoene.jack@gmail.com>
2024-04-17 22:27:48 -0500

    Some sort of error I cant pinpoint yet

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -3,6 +3,45 @@ import React, { useTransition, useState } from "react";
 import Image from "next/image";
 import TabButton from "./TabButton";
 
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+        <li>Item 4</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Item 5</li>
+        <li>Item 6</li>
+        <li>Item 7</li>
+        <li>Item 8</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Item 9</li>
+        <li>Item 10</li>
+        <li>Item 11</li>
+        <li>Item 12</li>
+      </ul>
+    ),
+  },
+];
+
 const AboutSection = () => {
   const [tab, setTab] = useState("skills");
   const [isPending, startTransition] = useTransition();
@@ -22,7 +61,7 @@ const AboutSection = () => {
           width={500}
           height={500}
         />
-        <div>
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base med:text-lg">
             A brief description of me yayayayayayayayayayayayayaya
@@ -50,6 +89,9 @@ const AboutSection = () => {
               Skills
               {"  "}
             </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
           </div>
         </div>
       </div>
---
+++ app/components/ProjectsSection.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import ProjectCard from "./ProjectCard";
+
+const ProjectsData = [
+  {
+    id: 1,
+    title: "React Portfolio Website",
+    description: "Project 1 Description",
+    image: "/projects/Me_sitting.jpg",
+    tag: ["All", "Web"],
+  },
+  {
+    id: 2,
+    title: "Photography Portfolio Website",
+    description: "Project 2 Description",
+    image: "/projects/Me_sitting.jpg",
+    tag: ["All", "Web"],
+  },
+  {
+    id: 3,
+    title: "Real Time Payment Application",
+    description: "Project 3 Description",
+    image: "/projects/Me_sitting.jpg",
+    tag: ["All", "Web"],
+  },
+  {
+    id: 4,
+    title: "Smartwatch Website",
+    description: "Project 4 Description",
+    image: "/projects/Me_sitting.jpg",
+    tag: ["All", "Web"],
+  },
+  {
+    id: 5,
+    title: "Lab Website",
+    description: "Project 5 Description",
+    image: "/projects/Me_sitting.jpg",
+    tag: ["All", "Web"],
+  },
+];
+
+const ProjectsSection = () => {
+  return (
+    <>
+      <h2 className="text-center text-4xl font-bold text-white mt-4">
+        My Projects
+      </h2>
+      <div className="grid md:grid-cold-3 gap-8 md:gap-12">
+        {ProjectsData.map((project) => (
+          <ProjectCard
+            key={project.id}
+            title={project.title}
+            description={project.description}
+            imgUrl={project.image}
+            tags={project.tag}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default ProjectsSection;
