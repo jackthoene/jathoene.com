496f2552cf33c4eeafc4ba56787d41ae11adaaa4
jackthoene <thoene.jack@gmail.com>
2024-04-19 18:49:52 -0500

    Done with front page

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -26,34 +26,36 @@ const HeroSection = () => {
       <div>{/* Overlay */}</div>
       <div className="grid grid-cols-1 sm:grid-cols-12 relative h-full z-10">
         <div className="col-span-7 place-self-center text-center sm:text-left">
-          <div className="absolute top-0 right-0 mt-20 mr-10">
+          <div className="text-right absolute top-0 right-0 mt-20 mr-10">
             <h1 className="text-white mb-4 text-xl lg:text-6xl font-extrabold">
               <span className="text-transparent bg-clip-text bg-gradient-to-r from-green-500 to-blue-500">
                 Hello, I'm{" "}
               </span>
               <br></br>
-              <TypeAnimation
-                sequence={[
-                  "Jack",
-                  1000,
-                  "A Mountaineer",
-                  1000,
-                  "A Marine",
-                  1000,
-                  "An Engineer",
-                  1000,
-                  "A Researcher",
-                  1000,
-                ]}
-                wrapper="span"
-                speed={50}
-                repeat={Infinity}
-              />
+              <div className="text-right">
+                <TypeAnimation
+                  sequence={[
+                    "Jack",
+                    1000,
+                    "A Mountaineer",
+                    1000,
+                    "A Marine",
+                    1000,
+                    "An Engineer",
+                    1000,
+                    "A Researcher",
+                    1000,
+                  ]}
+                  wrapper="span"
+                  speed={50}
+                  repeat={Infinity}
+                />
+              </div>
             </h1>
-            <p className="text-black text-base sm:text-lg mb-6 lg:text-xl">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            <p className="text-[#302e2e] text-base sm:text-lg mb-6 lg:text-xl">
+              I'm glad you made it here | Let's build something great together
             </p>
-            <div className="flex justify-center sm:justify-start">
+            <div className="flex justify-end">
               <button className="px-6 py-3 rounded-full mr-4 bg-gradient-to-br from-green-500 to-blue-500 hover:bg-slate-200 text-black">
                 Hire me
               </button>
